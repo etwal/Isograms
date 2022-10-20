81d9a1a74508710ece1cc5b9609661e7a2d18979
etwal <101393718+etwal@users.noreply.github.com>
2022-10-20 11:19:53 -0700

    ehsanDone

--- src/main/java/cpen221practice/IsogramChecker.java
+++ src/main/java/cpen221practice/IsogramChecker.java
@@ -3,7 +3,23 @@ package cpen221practice;
 class IsogramChecker {
 
     boolean isIsogram(String phrase) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        char []arr = phrase.toCharArray();
+
+
+
+
+        for (int i =0; i< arr.length; i++){
+            for (int j =0; j< arr.length; j++){
+                if(j != i && arr[i] != '-' && arr[j] != ' '){
+
+                    if(Character.toLowerCase(arr[i]) == Character.toLowerCase(arr[j]) ){
+                        return false;
+                    }
+
+                }
+            }
+        }
+        return true;
     }
 
 }
